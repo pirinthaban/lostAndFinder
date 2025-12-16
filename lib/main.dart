acae5feb16565edd98b0b02ef8fc81c884770e04
pirinthaban <www.pirinthaban@gmail.com>
2025-12-16 23:07:01 +0530

    Configure Firebase for production - enable authentication and Firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
-// import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-// import 'firebase_options.dart';
+import 'firebase_options.dart';
 import 'core/router/app_router.dart';
 import 'core/theme/app_theme.dart';
 // import 'core/utils/logger.dart';
@@ -10,10 +10,10 @@ import 'core/theme/app_theme.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Initialize Firebase (commented out until Firebase is configured)
-  // await Firebase.initializeApp(
-  //   options: DefaultFirebaseOptions.currentPlatform,
-  // );
+  // Initialize Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   // Set preferred orientations
   await SystemChrome.setPreferredOrientations([
